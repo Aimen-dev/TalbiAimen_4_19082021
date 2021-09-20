ab09d0832ce16230d85a152f9a5183c3dd42d6ec
Aïmen <aimen.talbi.pro@gmail.com>
2021-09-20 10:51:42 +0200

    modif

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close")
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,18 +22,52 @@ function launchModal() {
     modalbg.style.display = "block";
 }
 
-// Ici commence mon code
+// Click to leave
+closeBtn.addEventListener("click" , closeModal);
 
-// Close Modal form
-
-const modal = document.getElementById("Modal-content");
-
-document.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-function closeModal() {
-    modal.style.display = "none";
+// Close modal form
+function closeModal () {
+  modalbg.style.display="none"
 }
+
+
+/*----------------------- GESTION  DU FORMULAIRE -------------------------*/
+
+// Variables 
+const form = document.getElementById("formulaire");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const eMail = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const submit = document.querySelector('.btn-submit');
+const inputs = document.getElementsByTagName('input');
+let firstnameRegExp = new RegExp ('[0-9]');
+let lastnameRegExp = new RegExp ('[0-9]');
+
+// Variables champs d'erreurs
+const errorFirstName = document.querySelector(".errorFirstName");
+const errorLastName = document.querySelector(".errorLastName");
+const erroreMail = document.querySelector(".erroremail");
+const errorBirthdate = document.querySelector(".errorbirthdate");
+const errorQuantity = document.querySelector(".errorquantity");
+const checkIcon = document.querySelector(".checkbox-input");
+const errorCity = document.querySelector(".errorcity");
+const errorConditionUser = document.querySelector(".errorconditionuser");
+let erreur = 0;
+
+// validation au moment du clique sur "SUBMIT"
+form.addEventListener('submit', validate);
+
+// fonction valider le formulaire 
+function validate(e) {
+
+    e.preventDefault();
+  
+    let isFirstNameValid = false;
+    let isLastNameValid = false;
+    let isEmailValid = false;
+    let isBirthDateValid = false;
+    let isQuantityValid = false;
+    let isGetRadioButtonValid = false;
+    let isConditionUserValid = false;
