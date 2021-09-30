9627e3a5dcbb1056d6164ca4f23970cd0f774f63
Aïmen <aimen.talbi.pro@gmail.com>
2021-09-30 18:39:08 +0200

    optimisation CSS et JS

--- modal.js
+++ modal.js
@@ -12,9 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
-const fenetreEnvoie = document.querySelector('.fenetre-envoi');
-
-
+const fenetreEnvoie = document.querySelector(".fenetre-envoi");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,13 +23,12 @@ function launchModal() {
 }
 
 // Click to leave
-closeBtn.addEventListener("click" , closeModal);
+closeBtn.addEventListener("click", closeModal);
 
 // Close modal form
-function closeModal () {
-  modalbg.style.display="none"
+function closeModal() {
+    modalbg.style.display = "none";
 }
-
 
 /*----------------------- GESTION  DU FORMULAIRE -------------------------*/
 
@@ -42,30 +39,29 @@ const lastName = document.getElementById("last");
 const eMail = document.getElementById("email");
 const birthDate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
-const submit = document.querySelector('.btn-submit');
-const inputs = document.getElementsByTagName('input');
-let firstnameRegExp = new RegExp ('[0-9]');
-let lastnameRegExp = new RegExp ('[0-9]');
+const submit = document.querySelector(".btn-submit");
+const inputs = document.getElementsByTagName("input");
+let firstnameRegExp = new RegExp("[0-9]");
+let lastnameRegExp = new RegExp("[0-9]");
 
 // DOM champs d'erreurs
-const errorFirstName = document.querySelector(".errorFirstName");
-const errorLastName = document.querySelector(".errorLastName");
-const erroreMail = document.querySelector(".erroremail");
-const errorBirthdate = document.querySelector(".errorbirthdate");
-const errorQuantity = document.querySelector(".errorquantity");
+const errorFirstName = document.getElementById("errorFirstName");
+const errorLastName = document.getElementById("errorLastName");
+const erroreMail = document.getElementById("errorEmail");
+const errorBirthdate = document.getElementById("errorBirthdate");
+const errorQuantity = document.getElementById("errorQuantity");
 const checkIcon = document.querySelector(".checkbox-input");
-const errorCity = document.querySelector(".errorcity");
-const errorConditionUser = document.querySelector(".errorconditionuser");
+const errorCity = document.getElementById("errorCity");
+const errorConditionUser = document.getElementById("errorConditionUser");
 let erreur = 0;
 
 // validation au moment du clique sur "SUBMIT"
-form.addEventListener('submit', validate);
+form.addEventListener("submit", validate);
 
-// fonction valider le formulaire 
+// fonction valider le formulaire
 function validate(a) {
-
     a.preventDefault();
-  
+
     let isFirstNameValid = false;
     let isLastNameValid = false;
     let isEmailValid = false;
@@ -73,141 +69,146 @@ function validate(a) {
     let isQuantityValid = false;
     let isGetRadioButtonValid = false;
     let isConditionUserValid = false;
-  
+
     // Vérification du champ PRENOM
-    if(firstName.value.trim() == ""){
-      errorFirstName.innerHTML = "Vous devez écrire votre prénom.";
-      firstName.style.border = "2px solid #fe152f";
-      isFirstNameValid = false;
-    } else if (firstnameRegExp.test(firstName.value)){
-      errorFirstName.innerHTML = "Votre prénom ne doit pas comporter de chiffres ou de symboles.";
-      firstName.style.border = "2px solid #fe152f";
-      isFirstNameValid = false;
+    if (firstName.value.trim() == "") {
+        errorFirstName.innerHTML = "Vous devez écrire votre prénom.";
+        firstName.style.border = "2px solid #fe152f";
+        isFirstNameValid = false;
+    } else if (firstnameRegExp.test(firstName.value)) {
+        errorFirstName.innerHTML =
+            "Votre prénom ne doit pas comporter de chiffres ou de symboles.";
+        firstName.style.border = "2px solid #fe152f";
+        isFirstNameValid = false;
+    } else {
+        isFirstNameValid = true;
+        errorFirstName.innerHTML = "";
+        firstName.style.border = "";
     }
-    else {
-      isFirstNameValid = true;
-      errorFirstName.innerHTML = "";
-      firstName.style.border = "";
-    }
-  
+
     // Vérification du champ NOM
-    if(lastName.value.trim() ==""){
-      errorLastName.innerHTML = "Vous devez écrire votre nom.";
-      lastName.style.border ="2px solid #fe152f";
-      isLastNameValid = false;
-    } else if (lastnameRegExp.test(lastName.value)){
-      errorLastName.innerHTML = "Votre nom ne doit pas comporter de chiffres ou de symboles.";
-      lastName.style.border ="2px solid #fe152f";
-      isLastNameValid = false;
+    if (lastName.value.trim() == "") {
+        errorLastName.innerHTML = "Vous devez écrire votre nom.";
+        lastName.style.border = "2px solid #fe152f";
+        isLastNameValid = false;
+    } else if (lastnameRegExp.test(lastName.value)) {
+        errorLastName.innerHTML =
+            "Votre nom ne doit pas comporter de chiffres ou de symboles.";
+        lastName.style.border = "2px solid #fe152f";
+        isLastNameValid = false;
+    } else {
+        isLastNameValid = true;
+        errorLastName.innerHTML = "";
+        lastName.style.border = "";
     }
-      else {
-      isLastNameValid = true;
-      errorLastName.innerHTML = "";
-      lastName.style.border ="";
-    }
-    
+
     // Vérification du champ EMAIL
-    if(eMail.value.trim() ==""){
-      erroreMail.innerHTML = "Vous devez écrire votre e-mail.";
-      eMail.style.border ="2px solid #fe152f";
-      isEmailValid = false;
-    } else if (eMail.value.trim() === emailIsValid){
-      erroreMail.innerHTML = "L'e-mail saisi est incorrect.";
-      eMail.style.border ="2px solid #fe152f";
-      isEmailValid = false;
-    }else{
-      isEmailValid = true;
-      erroreMail.innerHTML = "";
-      eMail.style.border ="";
+    if (eMail.value.trim() == "") {
+        erroreMail.innerHTML = "Vous devez écrire votre e-mail.";
+        eMail.style.border = "2px solid #fe152f";
+        isEmailValid = false;
+    } else if (eMail.value.trim() === emailIsValid) {
+        erroreMail.innerHTML = "L'e-mail saisi est incorrect.";
+        eMail.style.border = "2px solid #fe152f";
+        isEmailValid = false;
+    } else {
+        isEmailValid = true;
+        erroreMail.innerHTML = "";
+        eMail.style.border = "";
     }
-  
+
     // Vérification du champ ANNIVERSAIRE
-    if(birthDate.value.trim() ==""){
-      errorBirthdate.innerHTML = "Vous devez renseigner votre date d'anniversaire.";
-      birthDate.style.border ="2px solid #fe152f";
-      isBirthDateValid = false;
-    }else{
-      isBirthDateValid = true;
-      errorBirthdate.innerHTML = "";
-      birthDate.style.border ="";
+    if (birthDate.value.trim() == "") {
+        errorBirthdate.innerHTML =
+            "Vous devez renseigner votre date d'anniversaire.";
+        birthDate.style.border = "2px solid #fe152f";
+        isBirthDateValid = false;
+    } else {
+        isBirthDateValid = true;
+        errorBirthdate.innerHTML = "";
+        birthDate.style.border = "";
     }
-  
+
     // Vérification du champ NOMBRE EVENEMENT
-    if(quantity.value.trim() ==""){
-      errorQuantity.innerHTML = "Vous devez préciser à combien d'évènement auquel vous avez participé.";
-      quantity.style.border ="2px solid #fe152f";
-      isQuantityValid = false;
-    }else{
-      isQuantityValid = true;
-      errorQuantity.innerHTML = "";
-      quantity.style.border ="";
-      }
-  
+    if (quantity.value.trim() == "") {
+        errorQuantity.innerHTML =
+            "Vous devez préciser à combien d'évènement auquel vous avez participé.";
+        quantity.style.border = "2px solid #fe152f";
+        isQuantityValid = false;
+    } else {
+        isQuantityValid = true;
+        errorQuantity.innerHTML = "";
+        quantity.style.border = "";
+    }
+
     // Vérification du champ CHOIX DE VILLES
-    if(getRadioButton(form.elements["location"]) == undefined){
-      errorCity.innerHTML = "Vous devez préciser dans quelle ville etait ces évènements.";
-      isGetRadioButtonValid = false;
-    }else{
-      isGetRadioButtonValid = true;
-      errorCity.innerHTML = "";
+    if (getRadioButton(form.elements["location"]) == undefined) {
+        errorCity.innerHTML =
+            "Vous devez préciser dans quelle ville etait ces évènements.";
+        isGetRadioButtonValid = false;
+    } else {
+        isGetRadioButtonValid = true;
+        errorCity.innerHTML = "";
     }
-  
+
     // Vérification du champ CONDITION UTILISATION
-    const conditionUser = document.getElementById('checkbox1');
+    const conditionUser = document.getElementById("checkbox1");
     let conditionsUserValue = conditionUser.checked;
-  
-    if (conditionsUserValue === false ){
-      errorConditionUser.innerHTML = "Vous devez accepter les conditions d'utilisateur"
-      isConditionUserValid = false;
-    } else{
-      conditionsUserValue = true; 
-      isConditionUserValid = true;
-      errorConditionUser.innerHTML = "";
-      
+
+    if (conditionsUserValue === false) {
+        errorConditionUser.innerHTML =
+            "Vous devez accepter les conditions d'utilisateur";
+        isConditionUserValid = false;
+    } else {
+        conditionsUserValue = true;
+        isConditionUserValid = true;
+        errorConditionUser.innerHTML = "";
     }
-  
+
     // FIN de fonction avec affichage du message de validation
-    if (isFirstNameValid && isLastNameValid && isEmailValid && isQuantityValid && isGetRadioButtonValid && isBirthDateValid && isConditionUserValid){
-     
-      messageConfirmation();
-      closeModal();
-      removeData();
+    if (
+        isFirstNameValid &&
+        isLastNameValid &&
+        isEmailValid &&
+        isQuantityValid &&
+        isGetRadioButtonValid &&
+        isBirthDateValid &&
+        isConditionUserValid
+    ) {
+        messageConfirmation();
+        closeModal();
+        removeData();
     }
-     
-  
-  };
-  
-  /*---- fonction qui compose la partie validate-----*/
+}
+
+/*---- fonction qui compose la partie validate-----*/
 
 // Fonction de vérification des emails
-function emailIsValid (email) {
-  return /\S+@\S+\.\S+/.test(email)
+function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email);
 }
-
 
 // Fonction de vérification des boutons radios
-function getRadioButton(radioBouton){
- let choix;
+function getRadioButton(radioBouton) {
+    let choix;
 
- for (let radio of radioBouton){
-   if (radio.checked){
-     choix = radio.value;
-   }
- }
- return choix;
+    for (let radio of radioBouton) {
+        if (radio.checked) {
+            choix = radio.value;
+        }
+    }
+    return choix;
 }
-
 
 function removeData() {
-firstName.value ="";
-lastName.value="";
-eMail.value="";
-birthDate.value="";
-quantity.value="";
-conditionUser.checked="";
+    firstName.value = "";
+    lastName.value = "";
+    eMail.value = "";
+    birthDate.value = "";
+    quantity.value = "";
+    conditionUser.checked = "";
 }
 
-
-function messageConfirmation(){
-fenetreEnvoie.style.display = "block";
+function messageConfirmation() {
+    fenetreEnvoie.style.display = "block";
 }
